f947ba83bfcd05d5808b4ffff5dd9e84d68e15b8
Manuel Prieto <prirodmanuel@gmail.com>
2023-05-25 17:38:02 +0200

    creada funcionalidad de suma

--- src/softwaregit/SoftwareGit.java
+++ src/softwaregit/SoftwareGit.java
@@ -16,5 +16,10 @@ public class SoftwareGit {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    int num1 = 0;
+    int num2 = 12;
     
+    int sumar = num1 + num2;
+    
+    System.out.println(sumar);
 }
